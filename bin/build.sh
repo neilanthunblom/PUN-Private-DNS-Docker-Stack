c9ce82fa5a36246af1459e3850972963d0b94202
Neilan Thunblom <neilan@thunblom.com>
2024-08-22 21:08:07 -0600

    add unbound config

--- bin/build.sh
+++ bin/build.sh
@@ -28,6 +28,49 @@ if [ ! -f "$SCRIPT_DIR/src/nginx/conf.d/nginx.crt" ] || [ ! -f "$SCRIPT_DIR/src/
   cp "$CERT_DIR/nginx.key" "$SCRIPT_DIR/src/nginx/conf.d/nginx.key"
 fi
 
+# UNBOUND CONFIGURATION -----------------------------------------------------
+UNBOUND_DIR="$SCRIPT_DIR/src/unbound"
+# Check if Unbound configuration exists. create if not
+if [ ! -f "$UNBOUND_DIR/unbound.conf" ]; then
+  mkdir -p "$UNBOUND_DIR"
+
+  # Create the Unbound configuration file
+  cat <<EOT > "$UNBOUND_DIR/unbound.conf"
+server:
+    verbosity: 1
+    num-threads: 2
+    interface: 0.0.0.0
+    access-control: 0.0.0.0/0 refuse
+    access-control: 127.0.0.0/8 allow
+    access-control: ::1 allow
+    port: 53
+    do-ip4: yes
+    do-ip6: no
+    do-udp: yes
+    do-tcp: yes
+    ssl-upstream: yes
+    auto-trust-anchor-file: "/opt/unbound/etc/unbound/root.key"
+    hide-identity: yes
+    hide-version: yes
+    harden-glue: yes
+    harden-dnssec-stripped: yes
+    use-caps-for-id: yes
+    cache-min-ttl: 3600
+    cache-max-ttl: 86400
+    prefetch: yes
+    rrset-cache-size: 100m
+    msg-cache-size: 50m
+    root-hints: "/opt/unbound/etc/unbound/root.hints"
+    include: "/opt/unbound/etc/unbound/unbound.conf.d/pi-hole.conf"
+EOT
+
+  # Download the root hints file
+  curl -o "$UNBOUND_DIR/root.hints" https://www.internic.net/domain/named.root
+
+  # Download the DNSSEC root trust anchor
+  curl -o "$UNBOUND_DIR/root.key" https://data.iana.org/root-anchors/root-anchors.xml
+fi
+
 # SERVICE -----------------------------------------------------
 SERVICE_FILE="/etc/systemd/system/pvt-dns.service"
 
